f9e0daa526d83321dbb5d6723c9b9f27bcee943d
Hiba Chaarani <hiba.chaarani@polymtl.ca>
2022-10-19 12:27:24 -0400

    chp 4 #2 complete

--- exercice.py
+++ exercice.py
@@ -4,18 +4,41 @@
 import random
 
 def get_first_part_of_name(name):
-	return ""
+	name = name.split("-")[0]
+	name = name[0].upper() + name[1:].lower()
+	return f'Bonjour, {name}'
 
 def get_random_sentence(animals, adjectives, fruits):
-	return ""
+	return f'Aujourd’hui, j’ai vu un {random.choice(animals)} s’emparer d’un panier {random.choice(adjectives)} plein de {random.choice(fruits)}.'
+
 
 def encrypt(text, shift):
-	return ""
+	letters = ""
+	for i in text:
+		if i.isalpha() == True:
+			i = i.upper()
+			if (ord(i) + shift) > 90:
+				letters += chr(ord(i) + shift - 26)
+			else:
+				letters += chr(ord(i) + shift)
+		else:
+			letters += i
+
+	return letters
 
 def decrypt(encrypted_text, shift):
-	return ""
+	lettres = ""
+	for i in encrypted_text:
+		if i.isalpha() == True:
+			i = i.upper()
+			if (ord(i) - shift) < 65:
+				lettres += chr(ord(i) - shift + 26)
+			else:
+				lettres += chr(ord(i) - shift)
+		else:
+			lettres += i
 
-
+	return lettres
 if __name__ == "__main__":
 	parrot = "jEaN-MARC"
 	print(f"Pour {parrot}, on a '{get_first_part_of_name(parrot)}'.")
